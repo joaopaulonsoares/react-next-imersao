5f033e8bb054125d1c1e204850e57a3345ec88e5
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-01-26 11:28:47 -0300

    Adding global head section and adding eslint

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Head from 'next/head';
+
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import Head from 'next/head'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -27,37 +28,38 @@ export const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-  <>
-    <Head> 
-        <meta property="og:url" content={"www.teste.url"} key="ogurl" />
+    <>
+      <Head>
+        <title>Quiz</title>
+        <meta property="og:url" content="www.teste.url" key="ogurl" />
         <meta property="og:image" content={db.bg} key="ogimage" />
         <meta property="og:site_name" content={db.title} key="ogsitename" />
         <meta property="og:title" content={db.title} key="ogtitle" />
         <meta property="og:description" content={db.description} key="ogdesc" />
-    </Head>
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>{db.description}</p>
-          </Widget.Content>
-        </Widget>
+      </Head>
+      <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <QuizLogo />
+          <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>{db.description}</p>
+            </Widget.Content>
+          </Widget>
 
-        <Widget>
-          <Widget.Content>
-            <h1>Quizes da Galera</h1>
+          <Widget>
+            <Widget.Content>
+              <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
-    </QuizBackground>
-  </>
+              <p>lorem ipsum dolor sit amet...</p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      </QuizBackground>
+    </>
   );
 }
